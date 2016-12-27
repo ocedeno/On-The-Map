38708f48d12c4855e324ec2c88e4a52775e089db
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2016-12-27 09:01:00 -0500

    Added Making Request aspect to Udacity Authorization

--- On the Map/UdacityClient.swift
+++ On the Map/UdacityClient.swift
@@ -28,24 +28,68 @@ class UdacityClient: NSObject {
     
     //MARK: POST
     
-    func authorizeUdacity(_ method: String, parameters: [String: String], jsonBody: String, completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
+    func taskForUdacityAuthentication(_ method: String, parameters: [String: String], jsonBody: String, completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
      
         //MARK: Setting Parameters
         UdacityClient.MethodParameters.Username = parameters["username"]
         UdacityClient.MethodParameters.UserPassword = parameters["password"]
         
         //MARK: Building URL, Configuring Request
-        let url = URL(string: UdacityClient.MethodKeys.NewSession)
+        let url = URL(string: UdacityClient.MethodKeys.NewSession)!
         
-        let request = NSMutableURLRequest(url: url!)
+        let request = NSMutableURLRequest(url: url)
         request.httpMethod = method
         request.addValue(UdacityClient.RequestValues.AppJSON, forHTTPHeaderField: UdacityClient.RequestValues.AcceptHeader)
         request.addValue(UdacityClient.RequestValues.AppJSON, forHTTPHeaderField: UdacityClient.RequestValues.ContentTypeHeader)
         request.httpBody = jsonBody.data(using: .utf8)
         
         //MARK: Making Request
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            //MARK: Error Handling
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForPOST(nil, NSError(domain: "taskForUdacityAuthentication", code: 1, userInfo: userInfo))
+            }
+            
+            //GUARD: Handling Error Return
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            // GUARD: Response Error Check
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            // GUARD: Data Error Check
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandlerForPOST)
+            
+        }
+        task.resume()
+        return task
+    }
+    
+    //Convert JSON Completion Handler
+    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
         
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
         
+        completionHandlerForConvertData(parsedResult, nil)
     }
     
 }
